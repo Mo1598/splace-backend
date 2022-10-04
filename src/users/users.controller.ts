31f2624bd588185a8f213a637a049aaf85c28597
Njehu <ngugipious1598@gmail.com>
2022-10-04 18:17:57 +0300

    Fixed RBAC

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/roles.guard';
 import { Roles } from 'src/decorators/roles.decorator';
 import { Role } from 'src/enums/role.enum';
 import { CreateUser } from './dtos/create-user.dto';
@@ -13,29 +14,32 @@ export class UsersController
 
     constructor(private userService: UsersService){}
 
-    @UseGuards(JwtAuthGuard)
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Get()
-    @Roles(Role.User)
+    @Roles(Role.Admin)
     async findAll() {
         var response = await this.userService.findAll();
         return response;
     }
 
-    @UseGuards(JwtAuthGuard)
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.Admin)
     @Get('/:id')
     async findone(@Param('id') id: string) {        
         var response = await this.userService.findOne(id);
         return response;
     }
 
-    @UseGuards(JwtAuthGuard)
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Post('/save')
     @Roles(Role.Admin)
     async save(@Body() user : CreateUser){
         const response = await this.userService.save(user);
+        return {"userId": response};
     }
 
-    @UseGuards(JwtAuthGuard)
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.Admin)
     @Delete(':id')
     remove(@Param('id') id: string) {
         return this.userService.remove(id);
